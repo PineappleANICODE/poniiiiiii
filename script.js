6ca1b0ba47f85170a93415433a5126901535bce6
PineappleANICODE <118062553+PineappleANICODE@users.noreply.github.com>
2023-02-18 13:39:51 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+function myFunction() {
+  var letter = document.getElementById("myInput").value;
+  var text;
+
+  
+  if (letter === "vapour trail") {
+    text = "Well done! You know your stuff!";
+    
+  
+  } else if (letter === "Vapour trail") {
+    text = "Well done! You know your stuff!"
+    
+    } else if (letter === "Vapour Trail") {
+    text = "Well done! You know your stuff!";
+    
+  
+  } else {
+    text = "Nope! You need to do your pony homework!";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
+
+
+setTimeout(function(){
+document.getElementById('div2').style.visibility = "visible";
+},1200);
+
+setTimeout(function(){
+document.getElementById('div3').style.visibility = "visible";
+},1400);
+
+setTimeout(function(){
+document.getElementById('div4').style.visibility = "visible";
+},1600);
+
+setTimeout(function(){
+document.getElementById('div5').style.visibility = "visible";
+},1800);
+
+setTimeout(function(){
+document.getElementById('div6').style.visibility = "visible";
+},2000);
+
+setTimeout(function(){
+document.getElementById('div7').style.visibility = "visible";
+},2200);
+
+setTimeout(function(){
+document.getElementById('div8').style.visibility = "visible";
+},2400);
+
+setTimeout(function(){
+document.getElementById('div9').style.visibility = "visible";
+},2800);
+
+setTimeout(function(){
+document.getElementById('div10').style.visibility = "visible";
+},3000);
+
+setTimeout(function(){
+document.getElementById('div11').style.visibility = "visible";
+},3200);
+setTimeout(function(){
+document.getElementById('div12').style.visibility = "visible";
+},3400);
+
+setTimeout(function(){
+document.getElementById('div12').style.visibility = "visible";
+},3600);
+
+setTimeout(function(){
+document.getElementById('div13').style.visibility = "visible";
+},3800);
+
+setTimeout(function(){
+document.getElementById('div14').style.visibility = "visible";
+},4000);
+
+setTimeout(function(){
+document.getElementById('div15').style.visibility = "visible";
+},4000);
